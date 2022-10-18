8b9a0817704fe361c58af8c4796486f9e9d2918b
Eric Hartanto <95305663+EH1121@users.noreply.github.com>
2022-10-18 21:19:26 +0700

    Add files via upload

--- introduction_to_rust/task_1/src/main.rs
+++ introduction_to_rust/task_1/src/main.rs
@@ -2,9 +2,16 @@ fn concat_string(param1:String, param2:String) -> String{
     return param1 + " " + &param2;
 }
 
+fn get_input() -> String{
+    let mut inputs = String::new();
+    std::io::stdin().read_line(&mut inputs).expect("Failed to get line");
+    let x = inputs.strip_suffix("\r\n").unwrap();
+    return String::from(x);
+}
+
 fn main() {
-    let param1:String = String::from("I love");
-    let param2:String = String::from("Rust");
+    let param1 = get_input();
+    let param2 = get_input();
     let concatenated = concat_string(param1, param2);
     println!("{concatenated}");
 }
